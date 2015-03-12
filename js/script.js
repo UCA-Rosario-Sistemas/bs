f6e81424f131e52369e051572b71b135ed256810
NicolasDF <ndelfiorentino@gmail.com>
2015-03-12 13:51:36 -0300

    Fixed "Cantidad" Box

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 var inicio=function	() {
+	alert('primera alerta');
 	
-	$(".cantidad").keyup(function (e) {
+	$('.cantidad').keyup(function (e) {
 		if($(this).val()!=''){
 			if(e.keyCode == 13){
 				alert('asdasd');
 				var id=$(this).attr('data-id');
 				var precio=$(this).attr('data-precio');
 				var cantidad=$(this).val();
-				//Encuentro el campo subtotal, y lo cambio
+				//Encuentro el la clase subtotal, y cambio su texto
 				$(this).parentsUntil('.producto').find('.subtotal').text('Subtotal: '+ (precio*cantidad));
 				//Usando ajax post = (ruta,{parametros},resultado)
-				$.post('js/modificarDatos.php'),{
+				$.post(('js/modificarDatos.php'),{
 					Id: id,
 					Precio: precio,
-					Cantidad: cantidad,
+					Cantidad: cantidad
 
 				}, function (e) {
 					$("#total").text('Total: '+e);
@@ -24,5 +25,6 @@ var inicio=function	() {
 			}
 		}
 	});
+	
 }
-$(document).on('ready',inicio);
+$(document).ready(inicio);
